a618337b20e0a04b8362e1554bb0aa9856e37324
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 19:14:22 +0200

    Implement RequestItem class with basic methods

---
+++ src/requestItem.cpp
@@ -0,0 +1,16 @@
+#include "RequestItem.h"
+
+FeedType RequestItem::getType() const
+{
+    return FeedType::Request;
+}
+
+std::string RequestItem::getTitle() const
+{
+    return "Request from " + sender;
+}
+
+std::string RequestItem::getDisplayColor() const
+{
+    return "blue";
+}
