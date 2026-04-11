02f11f42a642dab45909eb7306c08cdee8b49343
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 18:50:50 +0200

    Add enums for RequestStatus, Category, and Urgency

--- include/enums.h
+++ include/enums.h
@@ -1,1 +1,22 @@
+#pragma once
+#include <string>
 
+enum class RequestStatus {
+    Open,
+    Accepted,
+    Closed
+};
+
+enum class Category {
+    BorrowItem,
+    QuickQuestion,
+    TechHelp,
+    StudyPartner,
+    Other
+};
+
+enum class Urgency {
+    Low,
+    Medium,
+    High
+};
