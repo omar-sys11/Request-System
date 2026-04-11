f953915bb1d17489828cb223f7d6c6be655d2c3e
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 20:25:07 +0200

    Update feedWindow.cpp

--- src/feedWindow.cpp
+++ src/feedWindow.cpp
@@ -54,3 +54,37 @@ FeedWindow::FeedWindow(QWidget *parent)
     connect(addButton, &QPushButton::clicked,
             this, &FeedWindow::onAddClicked);
 }
+
+void FeedWindow::onAddClicked()
+{
+    std::string sender = senderInput->text().toStdString();
+    std::string content = contentInput->text().toStdString();
+
+    long long timestamp = QDateTime::currentSecsSinceEpoch();
+
+    std::unique_ptr<FeedItem> item;
+
+    QString type = typeBox->currentText();
+
+    if (type == "Message")
+    {
+        item = std::make_unique<MessageItem>(sender, content, timestamp);
+    }
+    else if (type == "Request")
+    {
+        item = std::make_unique<RequestItem>(sender, content, timestamp);
+    }
+    else if (type == "Accept")
+    {
+        item = std::make_unique<AcceptItem>(sender, content, timestamp);
+    }
+
+    if (item)
+    {
+        board.add(std::move(item));
+        refreshFeed();
+    }
+
+    senderInput->clear();
+    contentInput->clear();
+}
