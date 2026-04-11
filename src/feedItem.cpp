8c4124c71a3e4d2bf4b5965d6c7641790752d4aa
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 19:13:05 +0200

    Implement FeedItem constructor and getter methods

--- src/feedItem.cpp
+++ src/feedItem.cpp
@@ -1,1 +1,8 @@
+#include "FeedItem.h"
 
+FeedItem::FeedItem(std::string sender, std::string content, long long timestamp)
+    : sender(std::move(sender)), content(std::move(content)), timestamp(timestamp) {}
+
+std::string FeedItem::getSender() const { return sender; }
+std::string FeedItem::getContent() const { return content; }
+long long FeedItem::getTimestamp() const { return timestamp; }
