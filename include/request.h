1bcdb3cde2d078a9f5b2cf8a3fef4eff1365b258
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 18:54:09 +0200

    Implement Request class with attributes and methods
    Added a Request class with various attributes and methods.

--- include/request.h
+++ include/request.h
@@ -1,1 +1,43 @@
+#pragma once
 
+#include <string>
+#include <ctime>
+#include "enums.h"
+
+class Request {
+private:
+    std::string id;
+    std::string title;
+    std::string description;
+    Category category;
+    std::string location;
+    Urgency urgency;
+    RequestStatus status;
+    std::time_t timestamp;
+    std::string creatorId;
+
+public:
+    Request(const std::string& id,
+            const std::string& title,
+            const std::string& description,
+            Category category,
+            const std::string& location,
+            Urgency urgency,
+            const std::string& creatorId);
+
+    std::string getId() const;
+    std::string getTitle() const;
+    std::string getDescription() const;
+
+    Category getCategory() const;
+    Urgency getUrgency() const;
+    RequestStatus getStatus() const;
+
+    std::string getLocation() const;
+    std::time_t getTimestamp() const;
+
+    std::string getCreatorId() const;
+
+    void accept();
+    void close();
+};
