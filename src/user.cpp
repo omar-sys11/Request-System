3640abe3dcb5b818a998bb7fced67c70d2f4cd10
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-11 18:52:46 +0200

    Implement User class with basic functionality

--- src/user.cpp
+++ src/user.cpp
@@ -1,1 +1,24 @@
+#include "user.h"
 
+User::User(const std::string& id, const std::string& name)
+    : id(id), displayName(name), connected(true) {}
+
+std::string User::getId() const {
+    return id;
+}
+
+std::string User::getDisplayName() const {
+    return displayName;
+}
+
+bool User::isConnected() const {
+    return connected;
+}
+
+void User::setDisplayName(const std::string& name) {
+    displayName = name;
+}
+
+void User::setConnectionStatus(bool status) {
+    connected = status;
+}
