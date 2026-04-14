97e7968183589abb27893a6f8b0391982d885c57
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-14 22:08:01 +0200

    Implement createUser method in UserService

---
+++ src/UserService.cpp
@@ -0,0 +1,8 @@
+#include "UserService.h"
+#include <QUuid>
+
+User UserService::createUser(const std::string& name)
+{
+    std::string id = QUuid::createUuid().toString().toStdString();
+    return User(id, name);
+}
