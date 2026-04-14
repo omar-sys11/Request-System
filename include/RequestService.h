157f65819e79724110bd3cfbb1d2d9759b638120
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-14 22:52:10 +0200

    Modify addRequest to include ownerId parameter
    Added ownerId parameter to addRequest method.

--- include/RequestService.h
+++ include/RequestService.h
@@ -6,8 +6,9 @@
 class RequestService {
 public:
     void addRequest(const QString& title,
-                    const QString& category,
-                    const QString& location);
+                const QString& category,
+                const QString& location,
+                const QString& ownerId);
 
     std::vector<Request>& getRequests();
 
