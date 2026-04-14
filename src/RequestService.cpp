c9e13c105844203421862cf34946073df1bd3099
omar-sys11 <omar_quaqs@aucegypt.edu>
2026-04-14 23:01:44 +0200

    Update RequestService.cpp

--- src/RequestService.cpp
+++ src/RequestService.cpp
@@ -1,43 +1,18 @@
 #include "RequestService.h"
 #include <QDebug>
 
+RequestService::RequestService()
+{
+}
+
 void RequestService::addRequest(const QString& title,
                                 const QString& category,
-                                const QString& location)
+                                const QString& location,
+                                const QString& ownerId)
 {
-    Request r;
-    r.title = title;
-    r.category = category;
-    r.location = location;
-    r.ownerId = ownerId; 
-    r.status = RequestStatus::Open;
-
-    requests.push_back(r);
-
-}
-
-std::vector<Request>& RequestService::getRequests()
-{
-    return requests;
-}
-
-void RequestService::acceptRequest(int index, const QString& userId)
-{
-    if (index < 0 || index >= (int)requests.size()) return;
-
-    Request &req = requests[index];
-
-    if (req.ownerId == userId) {
-        qDebug() << "Cannot accept your own request";
-        return;
-    }
-
-    req.status = RequestStatus::Accepted;
-}
-
-void RequestService::closeRequest(int index)
-{
-    if (index < 0 || index >= (int)requests.size()) return;
-
-    requests[index].status = RequestStatus::Closed;
+    qDebug() << "Request added:";
+    qDebug() << "Title:" << title;
+    qDebug() << "Category:" << category;
+    qDebug() << "Location:" << location;
+    qDebug() << "OwnerId:" << ownerId;
 }
